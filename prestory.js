a9a89435f09f11c4c8cb8572a7b1eddef5847c8f
Mor <88229699+nemhiszem@users.noreply.github.com>
2022-03-07 22:53:02 +0100

    men

--- prestory.js
+++ prestory.js
@@ -1,15 +1,4 @@
-function timer(){
-    var sec = 5;
-    var timer = setInterval(function(){
-        for (var i = 0; i <5; i++)
-        {
-            sec--;
-            if (sec < 0) {
-                clearInterval(timer);
-            }
-            if (sec == 0) {
-            changebackgroundimage = 'speaker.png';
-            }
-        }
-    }, 1000);
-}
+function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ window.addEventListener("load",function() { changeBackground('red') });
