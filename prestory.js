d3dc963f2517666b296e93f65d6dae93fdeae1b4
Mor <88229699+nemhiszem@users.noreply.github.com>
2022-03-07 23:10:26 +0100

    changing background

--- prestory.js
+++ prestory.js
@@ -1,4 +1,9 @@
-function changeBackground(color) {
-    document.body.style.background = color;
- }
- window.addEventListener("load",function() { changeBackground('red') });
+var $el1;
+var $el2;
+
+setInterval(function() {
+    $el1 = $('.bg-container.active');
+    $el2 = $('.bg-container:not(.active');
+    $el1.removeClass('active');
+    $el2.addClass('active');
+}, 5000);
