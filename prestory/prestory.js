9069f3430a95325289f0f2a555b251875fb4a4da
Mor <88229699+nemhiszem@users.noreply.github.com>
2022-03-08 21:47:38 +0100

    animation

--- prestory/prestory.js
+++ prestory/prestory.js
@@ -14,13 +14,39 @@ setTimeout(function() {
 setTimeout(function() {
     $el4 = $('.bg-container4:not(.active');
     $el4.addClass('active');
+    const text = document.querySelector(".prestorytxt");
+    const strtext = text.textContent;
+    const splittext = strtext.split("");
+    text.textContent = "";
+
+    for (let i = 0; i < splittext.length; i++) {
+        text.innerHTML += "<span>" + splittext[i] + "</span>"
+    }
+
+    let char = 0;
+    let timer = setInterval(onTick, 50);
+
+    function onTick() {
+        const span = text.querySelectorAll('span')[char];
+        span.classList.add('fade');
+        char++;
+        if (char === splittext.length) {
+            complete();
+            return;
+        }
+    }
+
+    function complete() {
+        clearInterval(timer);
+        timer = null;
+    }
 }, 7000);
 
-document.getElementById("myButton").onclick = function () {
+document.getElementById("myButton").onclick = function() {
     location.href = "../solarsystem/solarsystem.html";
 }
 
-setTimeout(function () {
+setTimeout(function() {
     $button = $('.button_cont:not(.active');
     $button.addClass('active');
 }, 9000);
