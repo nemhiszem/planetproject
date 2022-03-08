64f9f4cff5756523fdc70cbb278555787eff5ed2
Borbola <55786923+Borbola@users.noreply.github.com>
2022-03-08 14:47:52 +0100

    gd

--- prestory/prestory.js
+++ prestory/prestory.js
@@ -2,6 +2,7 @@ var $el1;
 var $el2;
 var $el3;
 var $el4;
+var $button;
 
 setTimeout(function() {
     $el1 = $('.bg-container.active');
@@ -14,3 +15,12 @@ setTimeout(function() {
     $el4 = $('.bg-container4:not(.active');
     $el4.addClass('active');
 }, 7000);
+
+document.getElementById("myButton").onclick = function () {
+    location.href = "../solarsystem/solarsystem.html";
+}
+
+setTimeout(function () {
+    $button = $('.button_cont:not(.active');
+    $button.addClass('active');
+}, 9000);
