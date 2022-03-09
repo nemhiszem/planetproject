a48f4ca2d322700db9e453162b0b34ff8e7336eb
Mor <88229699+nemhiszem@users.noreply.github.com>
2022-03-09 13:07:15 +0100

    nem

--- prestory/prestory.js
+++ prestory/prestory.js
@@ -2,6 +2,7 @@ var $el1;
 var $el2;
 var $el3;
 var $el4;
+var $el5
 var $button;
 
 setTimeout(function() {
@@ -10,37 +11,48 @@ setTimeout(function() {
     $el2.addClass('active');
 }, 2000);
 
-
-setTimeout(function() {
-    $el4 = $('.bg-container4:not(.active');
-    $el4.addClass('active');
-    const text = document.querySelector(".prestorytxt");
-    const strtext = text.textContent;
-    const splittext = strtext.split("");
-    text.textContent = "";
-
-    for (let i = 0; i < splittext.length; i++) {
-        text.innerHTML += "<span>" + splittext[i] + "</span>"
-    }
-
-    let char = 0;
-    let timer = setInterval(onTick, 50);
-
-    function onTick() {
-        const span = text.querySelectorAll('span')[char];
-        span.classList.add('fade');
-        char++;
-        if (char === splittext.length) {
-            complete();
-            return;
-        }
-    }
-
-    function complete() {
-        clearInterval(timer);
-        timer = null;
-    }
+setTimeout(function nem() {
+    $el5 = $('.prestorytxt:not(.active');
+    $el5.addClass('active');
+    setTimeout(function() {
+        $el5 = $('.prestorytxt:not(.postActive');
+        $el5.addClass('postActive');
+    }, 12000);
 }, 7000);
+
+//setTimeout(function() {
+//$el4 = $('.bg-container4:not(.active');
+//$el4.addClass('active');
+
+/*
+const text = document.querySelector(".prestorytxt");
+const strtext = text.textContent;
+const splittext = strtext.split("");
+var comp = false;
+text.textContent = "";
+
+for (let i = 0; i < splittext.length; i++) {
+    text.innerHTML += "<span>" + splittext[i] + "</span>"
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if (char === splittext.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+} */
+//}, 7000);
 
 document.getElementById("myButton").onclick = function() {
     location.href = "../solarsystem/solarsystem.html";
